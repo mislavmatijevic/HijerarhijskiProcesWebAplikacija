cd6c4ff19fbd21eb7b6128c68f87725128ef49bc
Mislav Matijević <mislav.mim@gmail.com>
2022-06-04 19:21:04 +0200

    Enabled loading of default test values. Test values are from this AHP example: https://www.youtube.com/watch?v=J4T70o8gjlk

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,9 +4,18 @@
 	import ElementForm from '../lib/components/ElementForm.svelte';
 	import ElementsTable from '../lib/components/ElementsTable.svelte';
 
+	import testCriterias from '../lib/data/testCriterias.json';
+	import testPhones from '../lib/data/testPhones.json';
+
 	let criteraArray = ['Naziv'];
 	let observedElements = [];
 	let currentObservedElementIndex = 0;
+
+	const loadTestValues = () => {
+		criteraArray = testCriterias;
+		observedElements = testPhones;
+		currentObservedElementIndex = observedElements.length;
+	};
 </script>
 
 <h1>Analitički hijerarhijski proces</h1>
@@ -15,6 +24,8 @@
 		href="https://github.com/mislavmatijevic/HijerarhijskiProcesWebAplikacija">na GitHub-u</a
 	>.
 </p>
+
+<button on:click={loadTestValues}> Učitaj testne vrijednosti </button>
 
 <CriteriaInput bind:criteraArray />
 
