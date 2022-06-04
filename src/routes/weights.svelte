510ede7fcde41a2253968f8453cf2cdf7a445ee0
Mislav Matijević <mislav.mim@gmail.com>
2022-06-04 22:03:09 +0200

    Added selection of mutual criteria importance. Intensity of relative importance now selectable for each criteria pair.

--- src/routes/weights.svelte
+++ src/routes/weights.svelte
@@ -1,8 +1,14 @@
 <script>
+	import { getHtmlTagName } from '../lib/functions/getHtmlTagName';
+	import intensityOfRelativeImportance from '../lib/data/intensityOfRelativeImportance.json';
+
 	let criteriaArray;
 	try {
 		criteriaArray = JSON.parse(localStorage.getItem('criteriaArray'));
 	} catch {}
+
+	const getCurrentComparisonName = (criteriaMain, criteriaComparison) =>
+		getHtmlTagName(criteriaMain) + '_' + getHtmlTagName(criteriaComparison);
 </script>
 
 <h1>Određivanje prioriteta kriterija</h1>
@@ -10,5 +16,29 @@
 {#if criteriaArray === undefined}
 	<span>Pričekajte...</span>
 {:else}
-	<span>{criteriaArray[0]}</span>
+	{#each criteriaArray as criteriaMain, indexMain}
+		{#if indexMain !== 0}
+			{#each criteriaArray as criteriaComparison, indexComparison}
+				{#if indexMain < indexComparison}
+					<label
+						for={'select_importance_' + getCurrentComparisonName(criteriaMain, criteriaComparison)}
+						>Ocjena važnosti kriterija "{@html '<strong>' + criteriaMain + '</strong>'}" s
+						kriterijem "{@html '<strong>' + criteriaComparison + '</strong>'}"</label
+					>
+					<select
+						id={'select_importance_' + getCurrentComparisonName(criteriaMain, criteriaComparison)}
+					>
+						{#each intensityOfRelativeImportance as importanceValue, index}
+							{#if index === 5}
+								<optgroup />
+							{/if}
+							<option>{importanceValue.definition} ({importanceValue.intensity})</option>
+						{/each}
+					</select>
+					<br />
+				{/if}
+			{/each}
+		{/if}
+	{/each}
 {/if}
+<!-- Loaded content -->
