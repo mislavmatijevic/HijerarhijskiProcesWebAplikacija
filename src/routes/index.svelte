f1fb3a49331054e98f1146ec1ae355d9188bdae2
Mislav Matijević <mislav.mim@gmail.com>
2022-06-04 16:30:32 +0200

    Elements can now be selected and changed after being entererd in the table.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,19 +1,36 @@
 <script>
 	let kriteriji = ['Naziv'];
 	let vrijednosti = [];
+	let trenutnaVrijednost = 0;
 
 	const dajHtmlNaziv = (znakovniNiz) => {
 		return 'input_' + znakovniNiz.trim().replace(' ', '_').toLowerCase();
 	};
 
-	const pohraniNovuVrijednost = (event) => {
+	const pohraniVrijednost = (event) => {
 		event.preventDefault();
 		const novaVrijednost = {};
 		kriteriji.forEach((kriterij) => {
 			novaVrijednost[kriterij] = document.querySelector(`#${dajHtmlNaziv(kriterij)}`).value;
 		});
-		console.log(novaVrijednost);
-		vrijednosti = [...vrijednosti, novaVrijednost];
+
+		if (trenutnaVrijednost < 0) {
+			trenutnaVrijednost = 0;
+		}
+
+		if (trenutnaVrijednost == vrijednosti.length) {
+			vrijednosti = [...vrijednosti, novaVrijednost];
+			trenutnaVrijednost = vrijednosti.length;
+		} else if (trenutnaVrijednost < vrijednosti.length) {
+			vrijednosti[trenutnaVrijednost] = novaVrijednost;
+		}
+	};
+
+	const uskladiInputeZaTrenutniElement = () => {
+		kriteriji.forEach((kriterij) => {
+			document.querySelector(`#${dajHtmlNaziv(kriterij)}`).value =
+				vrijednosti[trenutnaVrijednost][kriterij];
+		});
 	};
 </script>
 
@@ -30,14 +47,41 @@
 </button>
 
 <form>
-	<h2>Nova vrijednost</h2>
+	<h2>
+		{#if trenutnaVrijednost == vrijednosti.length}
+			Nova
+		{:else}
+			{trenutnaVrijednost + 1}
+		{/if} vrijednost
+	</h2>
+	<button
+		on:click={(e) => {
+			e.preventDefault();
+			trenutnaVrijednost > 0 && trenutnaVrijednost--;
+			uskladiInputeZaTrenutniElement();
+		}}>&lt;-</button
+	>
+	<button
+		on:click={(e) => {
+			e.preventDefault();
+			trenutnaVrijednost < vrijednosti.length && trenutnaVrijednost++;
+			uskladiInputeZaTrenutniElement();
+		}}>-&gt;</button
+	>
+	<br />
 	{#each kriteriji as kriterij}
 		<label for={dajHtmlNaziv(kriterij)}>{kriterij}: </label>
 		<input id={dajHtmlNaziv(kriterij)} />
 		<br />
 	{/each}
 	{#if kriteriji.length > 0}
-		<button on:click={pohraniNovuVrijednost}>Dodaj novi element u tablicu</button>
+		<button on:click={pohraniVrijednost}>
+			{#if trenutnaVrijednost == vrijednosti.length}
+				Dodaj novi element u tablicu
+			{:else}
+				Pohrani {trenutnaVrijednost + 1} element.
+			{/if} vrijednost
+		</button>
 	{/if}
 </form>
 
